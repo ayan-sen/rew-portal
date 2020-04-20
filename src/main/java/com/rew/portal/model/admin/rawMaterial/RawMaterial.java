42024d27cdf0c230b148dd3853c27e13d5f97c1b
Ayan Sen <ayan.sen@live.com>
2020-04-21 02:39:06 +0530

    mapping added

--- src/main/java/com/rew/portal/model/admin/rawMaterial/RawMaterial.java
+++ src/main/java/com/rew/portal/model/admin/rawMaterial/RawMaterial.java
@@ -1,34 +1,72 @@
 package com.rew.portal.model.admin.rawMaterial;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+import com.rew.portal.model.admin.unit.Unit;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
-@Entity(name="raw_material")
+@Entity(name = "raw_material")
 public class RawMaterial {
 
 	@Id
-	@Column(name="code")
+	@Column(name = "code")
 	private String code;
-	
-	@Column(name="name")
+
+	@Column(name = "name")
 	private String name;
-	
-	@Column(name="description", nullable=true)
+
+	@Column(name = "description", nullable = true)
 	private String description;
-	
-	@Column(name="unitId")
+
+	@Column(name = "unitId")
 	private String unitId;
-	
-	@Column(name="isActive")
+
+	@Column(name = "isActive")
 	private Boolean isActive;
+
+	@Transient
+	private String unitName;
+	
+	@NotFound(action=NotFoundAction.IGNORE)
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="unitId",insertable=false,updatable=false)
+	private Unit unit;
+
+	public String getUnitName() {
+		if (Objects.nonNull(this.unit)) {
+			return this.unit.getUnitName();
+		}
+		return this.unitName;
+	}
+	
+	/*public String getUnitId() {
+		if (Objects.nonNull(this.unit)) {
+			return this.unit.getUnitId();
+		}
+		return null;
+	}*/
+
+	@Override
+	public String toString() {
+		return "RawMaterial [code=" + code + ", name=" + name
+				+ ", description=" + description + ", isActive=" + isActive
+				+ ", unitName=" + unitName + "]";
+	}
 }
--- src/main/java/com/rew/portal/model/admin/unit/Unit.java
+++ src/main/java/com/rew/portal/model/admin/unit/Unit.java
@@ -1,25 +1,34 @@
 package com.rew.portal.model.admin.unit;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import com.rew.portal.model.admin.rawMaterial.RawMaterial;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Entity(name="unit")
+@Entity(name = "unit")
 public class Unit {
 
 	@Id
-	@Column(name="unitId")
+	@Column(name = "unitId")
 	private String unitId;
-	
-	@Column(name="unitName")
+
+	@Column(name = "unitName")
 	private String unitName;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "unitId")
+	private RawMaterial rawMaterial;
 }
