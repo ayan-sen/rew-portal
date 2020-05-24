8d74b2c7f9621bee8cc327a19a7e6108afec040d
Ayan Sen <ayan.sen@live.com>
2020-05-24 20:56:09 +0530

    modified

--- src/main/java/com/rew/portal/controller/transaction/inventory/InventoryController.java
+++ src/main/java/com/rew/portal/controller/transaction/inventory/InventoryController.java
@@ -1,8 +1,26 @@
 package com.rew.portal.controller.transaction.inventory;
 
+import java.util.Collection;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rew.portal.service.transaction.inventory.InventoryService;
 
 @RestController
 public class InventoryController {
 
+	@Resource
+	private InventoryService inventoryService;
+
+	@SuppressWarnings("rawtypes")
+	@GetMapping("/transaction/inventory")
+	public ResponseEntity<Map<String, Collection>> getStatus() {
+		return new ResponseEntity<Map<String, Collection>>(inventoryService.getStatus(), HttpStatus.OK);
+	}
 }
---
+++ src/main/java/com/rew/portal/service/transaction/inventory/InventoryService.java
@@ -0,0 +1,67 @@
+package com.rew.portal.service.transaction.inventory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.rew.portal.model.transaction.inventory.InventoryRecord;
+import com.rew.portal.repository.transaction.inventoryRecord.InventoryRecordRepository;
+
+@Service
+public class InventoryService {
+
+	@Resource
+	private InventoryRecordRepository inventoryRecordRepository;
+
+	@SuppressWarnings("rawtypes")
+	public Map<String, Collection> getStatus() {
+		List<InventoryRecord> records = inventoryRecordRepository.findAll();
+		Map<String, Map<String, Map<String, Double>>> grouping = records
+				.stream()
+				.collect(
+						Collectors.groupingBy(
+								InventoryRecord::getRawMaterialCode,
+								Collectors.groupingBy(
+										InventoryRecord::getSiteId,
+										Collectors
+												.groupingBy(
+														InventoryRecord::getInOutFlag,
+														Collectors
+																.summingDouble(InventoryRecord::getQuantity)))));
+
+		Set<String> labels = grouping.keySet();
+		List<Double> dumdumQuantity = new ArrayList<>();
+		List<Double> singurQuantity = new ArrayList<>();
+
+		labels.stream().forEachOrdered(
+				lebel -> {
+					Map<String, Map<String, Double>> quantityMap = grouping
+							.get(lebel);
+					Map<String, Double> dumdumQuantityMap = quantityMap
+							.getOrDefault("DUMDUM", Collections.emptyMap());
+					Map<String, Double> singurQuantityMap = quantityMap
+							.getOrDefault("SINGUR", Collections.emptyMap());
+					dumdumQuantity.add(dumdumQuantityMap
+							.getOrDefault("IN", 0.0)
+							- dumdumQuantityMap.getOrDefault("OUT", 0.0));
+					singurQuantity.add(singurQuantityMap
+							.getOrDefault("IN", 0.0)
+							- singurQuantityMap.getOrDefault("OUT", 0.0));
+				});
+
+		Map<String, Collection> chartData = new HashMap<>();
+		chartData.put("labels", labels);
+		chartData.put("dumdum", dumdumQuantity);
+		chartData.put("singur", singurQuantity);
+		return chartData;
+	}
+}
