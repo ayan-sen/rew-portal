cb20824c3ed8d93cccd20bff06a1e62e34d39412
Ayan Sen <ayan.sen@live.com>
2020-06-08 22:23:48 +0530

    modified

--- src/main/java/com/rew/portal/config/CorsConfiguration.java
+++ src/main/java/com/rew/portal/config/CorsConfiguration.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 	@Value("${ui.context-path}")
 	private String uiContextPath;
@@ -18,4 +18,11 @@ public class CorsConfiguration implements WebMvcConfigurer {
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
 				
 	}
+	
+	 /*@Override
+     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+             registry.addResourceHandler("/resources/**")
+                     .addResourceLocations("/public", "/static");
+                     
+     }*/
 }
