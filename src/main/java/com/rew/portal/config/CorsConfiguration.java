c40333f6130ec61768c639b1ddd5dd186c92112a
Ayan Sen <ayan.sen@live.com>
2022-08-09 12:46:31 +0530

    added ui

--- src/main/java/com/rew/portal/config/CorsConfiguration.java
+++ src/main/java/com/rew/portal/config/CorsConfiguration.java
@@ -3,8 +3,6 @@ package com.rew.portal.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
