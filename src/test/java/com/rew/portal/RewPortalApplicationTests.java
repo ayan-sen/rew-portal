944321c5c8d4ea49f9c437c34f12eada5effa906
Ayan Sen <ayan.sen@live.com>
2022-08-12 01:31:01 +0530

    added profile in procfile

--- src/test/java/com/rew/portal/RewPortalApplicationTests.java
+++ src/test/java/com/rew/portal/RewPortalApplicationTests.java
@@ -1,13 +1,13 @@
-package com.rew.portal;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RewPortalApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.rew.portal;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RewPortalApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
